31b0e4a3b97a788f969b4d8cd87b0da5c9a27c4f
Lucas Procopio <lucas@eadskill.com.br>
2019-08-22 11:57:37 -0300

    feat: Provider appointment notification
    Add mongodb schema to handle provider appointment notification

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,9 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -91,6 +92,17 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(startHour, "MMMM dd', at' H:mm'h'");
+
+    await Notification.create({
+      content: `New appointment for the user ${user.name} on ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
