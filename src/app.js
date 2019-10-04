867f553dfc07b2deda90154178b5e63ab223cb91
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-10-04 15:23:24 -0300

    :sparkles: chore: add api access point

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import routes from './routes';
-
 import './database';
 
 class App {
@@ -13,6 +13,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
